4675698df42009fe3fc123c8c840d3ddc85f9754
enovak89 <enovak89@mail.ru>
2023-03-25 22:32:41 +0300

    Init commit

---
+++ src/main/java/com/example/hwtests/HwTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwTestsApplication.class, args);
+    }
+
+}
